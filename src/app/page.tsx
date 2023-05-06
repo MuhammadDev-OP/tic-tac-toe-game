36755ac369424bd5cb361071a663dca05c6c3ce4
Muhammad B <muhammadbangash190@gmail.com>
2023-05-07 00:02:16 +0500

    ewdwdsa

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,18 @@ interface SquareType {
   value: string | number | any;
 }
 
-function Square() {
+function Square({value} : SquareType) {
 
-  const [squares, setSquares] = useState(Array(9).fill(null));
-
+ 
   return (
-     <button  className="square">{squares}</button>
+     <button  className="square">{value}</button>
 
   )
 }
 
 export default function Home() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
   return (
     <>
       <div className="text-center mt-44 bg-stone-200 ">
@@ -27,21 +28,21 @@ export default function Home() {
       </div>
       <div className="ml-5 mt-5">
       <div className="table-row-group">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares['1']} />
+        <Square value={squares['2']} />
+        <Square value={squares['3']} />
         </div>
         <div className="table-row-group">
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares['4']} />
+        <Square value={squares['5']} />
+        <Square value={squares['6']} />
         </div>
         <div className="table-row-group">
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares['7']} />
+        <Square value={squares['8']} />
+        <Square value={squares['9']} />
         </div>
       </div>
     </>
