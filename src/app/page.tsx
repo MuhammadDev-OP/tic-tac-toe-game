99a3dd46ae3e2af0444326af50b1b3590a9718f1
Muhammad B <muhammadbangash190@gmail.com>
2023-05-06 20:26:46 +0500

    6th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+'use client';
+
 import Image from "next/image";
 
-export function Square() {
-  return <button className="square">1</button>;
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
 
 export default function Home() {
@@ -14,21 +16,21 @@ export default function Home() {
       </div>
       <div className="ml-5 mt-5">
       <div className="table-row-group">
-        <Square />
-        <Square />
-        <Square />
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
         </div>
         <div className="table-row-group">
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
         </div>
         <div className="table-row-group">
 
-        <Square />
-        <Square />
-        <Square />
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
         </div>
         </div>
     </>
